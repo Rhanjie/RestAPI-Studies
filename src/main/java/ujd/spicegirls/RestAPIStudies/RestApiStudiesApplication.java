9969106deb78f9e53fda0adde13b40fc3cbf6e88
RhAnjiE <irhanjie@gmail.com>
2022-02-18 00:02:03 +0100

    Add initial spring project

---
+++ src/main/java/ujd/spicegirls/RestAPIStudies/RestApiStudiesApplication.java
@@ -0,0 +1,13 @@
+package ujd.spicegirls.RestAPIStudies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiStudiesApplication.class, args);
+	}
+
+}
