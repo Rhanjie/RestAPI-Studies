a918374d4e1f8b046db63c1bc5cca9519fec14e9
RhAnjiE <irhanjie@gmail.com>
2022-02-20 02:05:56 +0100

    Add first test controller and service

---
+++ src/main/java/ujd/spicegirls/RestAPIStudies/controllers/HelloController.java
@@ -0,0 +1,18 @@
+package ujd.spicegirls.RestAPIStudies.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ujd.spicegirls.RestAPIStudies.services.HelloService;
+
+@RestController
+@RequiredArgsConstructor
+public class HelloController {
+    private final HelloService helloService;
+
+    @GetMapping("/")
+    public String hello()  {
+        return helloService.hello();
+    }
+}
