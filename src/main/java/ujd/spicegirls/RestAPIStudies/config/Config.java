02c3462bc7629c34621c3d097134e976104c9105
RhAnjiE <irhanjie@gmail.com>
2022-02-27 19:51:54 +0100

    Configure config class to use tokens by endpoints

--- src/main/java/ujd/spicegirls/RestAPIStudies/config/Config.java
+++ src/main/java/ujd/spicegirls/RestAPIStudies/config/Config.java
@@ -5,10 +5,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -18,6 +26,29 @@ public class Config {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .paths(PathSelectors.regex("^(?!/(error).*$).*$"))
+                .build()
+                .securitySchemes(Collections.singletonList(createSchema()))
+                .securityContexts(Collections.singletonList(createContext()));
+    }
+
+    private SecurityContext createContext() {
+        return SecurityContext.builder()
+                .securityReferences(createRef())
+                .forPaths(PathSelectors.any())
                 .build();
+    }
+
+    private List<SecurityReference> createRef() {
+        var authorizationScope = new AuthorizationScope("global", "accessEverything");
+        var authorizationScopes = new AuthorizationScope[1];
+
+        //Convert to array
+        authorizationScopes[0] = authorizationScope;
+
+        return Collections.singletonList(new SecurityReference("apiKey", authorizationScopes));
+    }
+
+    private SecurityScheme createSchema() {
+        return new ApiKey("apiKey", "Authorization", "header");
     }
 }
