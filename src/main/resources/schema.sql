53834c9e2447b8f04272cb3b5329c6c1d1876dd9
EsmiraKai <natka199419@gmail.com>
2024-01-21 22:25:12 +0100

    Change database schema and default data

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,32 +2,32 @@ CREATE TABLE `user` (
     `id` INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     `name` varchar(20) DEFAULT NULL,
     `surname` varchar(20) DEFAULT NULL,
-    `pesel` int(11) DEFAULT NULL
+    `pesel` int(11) DEFAULT NULL,
+    `age` smallint(3) DEFAULT NULL
 );
 
 CREATE TABLE `payment` (
     `id` INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     `id_user` INTEGER NOT NULL,
-    `name` varchar(20) DEFAULT NULL,
+    `id_book` INTEGER NOT NULL,
     `borrow_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     `expiration_date` DATETIME NOT NULL,
     FOREIGN KEY (id_user) REFERENCES user(id)
 );
 
-CREATE TABLE `type` (
+CREATE TABLE `genre` (
     `id` INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
     `name` varchar(10) DEFAULT NULL,
-    `weight` decimal(3,2) DEFAULT NULL
+    `only_for_adults` BOOL DEFAULT FALSE
 );
 
-CREATE TABLE `equipment` (
+CREATE TABLE `book` (
     `id` INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    `id_type` INTEGER NOT NULL,
-    `model` varchar(7) DEFAULT NULL,
-    `producer` varchar(9) DEFAULT NULL,
-    `maximum_load` smallint(6) DEFAULT NULL,
-    `weight` decimal(3,2) DEFAULT NULL,
-    `range` varchar(2) DEFAULT NULL,
-    `battery_capacity` varchar(3) DEFAULT NULL,
-    FOREIGN KEY (id_type) REFERENCES type(id)
+    `id_genre` INTEGER NOT NULL,
+    `title` varchar(30) DEFAULT NULL,
+    `author` varchar(20) DEFAULT NULL,
+    `publisher` varchar(20) DEFAULT NULL,
+    `year` smallint(4) DEFAULT NULL,
+    `condition` varchar(20) DEFAULT NULL,
+    FOREIGN KEY (id_genre) REFERENCES genre(id)
 );
